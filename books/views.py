d4afc8ab7cb1aabd261c9fda59091be0aeff39d0
UncleAyo <ayosamobitade@gmail.com>
2023-06-13 17:48:23 +0100

    library website

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from .models import Book
+from django.views.generic import ListView
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+
+
